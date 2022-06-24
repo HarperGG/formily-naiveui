423045e534ac9e0b9de3f17c732de9e12825f729
gang.yang <gang.yang@tusen.ai>
2022-06-24 18:02:02 +0800

    feat(form-button-group): add form-button-group component

---
+++ packages/components/src/form-button-group/index.ts
@@ -0,0 +1,67 @@
+import { h } from '@formily/vue'
+import { defineComponent } from 'vue'
+import { Space } from '../space'
+import { FormBaseItem } from '../form-item'
+import { stylePrefix } from '../__builtins__'
+
+export const FormButtonGroup = defineComponent({
+  name: 'FFormButtonGroup',
+  props: {
+    align: {
+      type: String,
+      default: 'left',
+    },
+    gutter: {
+      type: Number,
+      default: 8,
+    },
+    alignFormItem: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  setup(props, { slots, attrs }) {
+    const prefixCls = `${stylePrefix}-form-button-group`
+    return () => {
+      if (props.alignFormItem) {
+        return h(
+          FormBaseItem,
+          {
+            colon: false,
+            label: ' ',
+            ...attrs,
+            style: {
+              margin: 0,
+              padding: 0,
+              width: '100%',
+            },
+          },
+          {
+            default: () => h(Space, { size: props.gutter }, slots),
+          }
+        )
+      } else {
+        return h(
+          Space,
+          {
+            ...attrs,
+            class: [prefixCls],
+            style: {
+              justifyContent:
+                props.align === 'left'
+                  ? 'flex-start'
+                  : props.align === 'right'
+                  ? 'flex-end'
+                  : 'center',
+              display: 'flex',
+            },
+            size: props.gutter,
+          },
+          slots
+        )
+      }
+    }
+  },
+})
+
+export default FormButtonGroup
